ef3a13f2c526770d19375e6d63d753a319ebfd23
khalib <caleb.strumms@gmail.com>
2013-03-01 05:24:17 +0000

    Cleaned out repo.

--- meadows/application/logs/2012/11/28.php
+++
@@ -1,9 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.'); ?>
-
-2012-11-28 08:43:13 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL posts was not found on this server. ~ SYSPATH/classes/kohana/request/client/internal.php [ 87 ]
-2012-11-28 08:43:13 --- STRACE: HTTP_Exception_404 [ 404 ]: The requested URL posts was not found on this server. ~ SYSPATH/classes/kohana/request/client/internal.php [ 87 ]
---
-#0 /Users/caleb/Sites/meadowsmillbrae.com/docroot/system/classes/kohana/request/client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
-#1 /Users/caleb/Sites/meadowsmillbrae.com/docroot/system/classes/kohana/request.php(1154): Kohana_Request_Client->execute(Object(Request))
-#2 /Users/caleb/Sites/meadowsmillbrae.com/docroot/index.php(109): Kohana_Request->execute()
-#3 {main}
