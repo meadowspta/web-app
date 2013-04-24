7738800fdf1e86b50de4a4d5c01253d002696940
khalib <caleb.strumms@gmail.com>
2013-04-23 22:41:26 -0700

    Temporary homepage

--- pta/docroot/index.php
+++ pta/docroot/index.php
@@ -1,1 +1,132 @@
-Meadows PTA home
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Meadows Elementary School PTA</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <link type="text/css" rel="stylesheet" media="all" href="/static/css/bootstrap.css?v=0.1" />
+        <link type="text/css" rel="stylesheet" media="all" href="/static/css/meadows.css?v=0.1" />
+
+        <script type="text/javascript">var MeadowsPTA = {};</script>
+        <script type="text/javascript" src="/static/js/jquery-1.8.1.min.js?v=0.1"></script>
+        <script type="text/javascript" src="/static/js/underscore-min.js?v=0.1"></script>
+        <script type="text/javascript" src="/static/js/meadows.js?v=0.1"></script>
+
+        <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+        <!-- Le fav and touch icons -->
+        <link rel="shortcut icon" href="/static/ico/favicon.ico">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/static/ico/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/static/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/static/ico/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="/static/ico/apple-touch-icon-57-precomposed.png">
+    </head>
+
+    <body>
+        <div id="fb-root"></div>
+
+    <div id="header-background"></div>
+    <div id="header">
+        <div class="container">
+            <div class="row">
+                <div class="span4">
+                    <div id="logo"><a href="http://local.meadowsmillbrae.com/"><img src="/static/images/template/logo_type.png"></a></div>
+                </div>
+                <div class="span8">
+                    <ul class="navigation">
+                        <li><a href="http://local.meadowsmillbrae.com/news">News</a></li>
+                        <li><a href="http://local.meadowsmillbrae.com/calendar">Calendar</a></li>
+                        <li><a href="http://meadowspta.org/" target="_blank">PTA</a></li>
+                        <li class="last"><a href="http://local.meadowsmillbrae.com/meals/menu">Food Services</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="contact-info pull-right">
+                    <a href="https://maps.google.com/maps?q=1101+Helen+Drive+Millbrae,+CA+94030&amp;hl=en&amp;ll=37.602094,-122.418487&amp;spn=0.004573,0.016158&amp;sll=37.269174,-119.306607&amp;sspn=10.362441,16.54541&amp;hnear=1101+Helen+Dr,+Millbrae,+San+Mateo,+California+94030&amp;t=m&amp;z=16&amp;layer=c&amp;cbll=37.602093,-122.418482&amp;panoid=eemM59uql4Hu_seTg4cBUg&amp;cbp=11,217.17,,0,-4.18" target="_blank">1101 Helen Drive Millbrae, CA 94030</a> &middot (650) 583-7590
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div id="page" class="row">
+            <div id="main" class="span8">
+                <h2>Meadows PTA Auction Dinner</h2>
+                <div>
+                    <img src="http://www.meadowsmillbrae.com/static/images/posts/516f7b314792d81f60000001_medium.jpg" class="main image-medium" align="right">
+                    <div>
+                        <img src="http://www.meadowsmillbrae.com/static/images/icons/pdf_small.png" title="" alt="">&nbsp;<a href="http://www.meadowsmillbrae.com/static/files/posts/auction_flyer.pdf" title="" target="_blank">Download the PTA Auction Form<br><br></a><b style="text-align: -webkit-center;">Friday, April 26th, 2013<br><br>Dinner:</b><span style="text-align: -webkit-center;">&nbsp;5:30pm or 6:15pm seating time (space limited)</span><br style="text-align: -webkit-center;"><br style="text-align: -webkit-center;"><b style="text-align: -webkit-center;">Silent Auction:</b><span style="text-align: -webkit-center;">&nbsp;5:30pm - 7:00pm</span><br style="text-align: -webkit-center;"><br style="text-align: -webkit-center;"><b style="text-align: -webkit-center;">Live Auction:</b><span style="text-align: -webkit-center;">&nbsp;7:30pm Classroom items &amp; 8:15pm Public</span><br style="text-align: -webkit-center;"><br style="text-align: -webkit-center;"><b style="text-align: -webkit-center;">Raffle Tickets:</b><span style="text-align: -webkit-center;">&nbsp;$2 each or 12 for $20 for many prizes including an Apple iPad Mini!</span><br style="text-align: -webkit-center;"><br style="text-align: -webkit-center;"><span style="text-align: -webkit-center;">Meadows Pre-sale - February 25th to April 1st</span><br style="text-align: -webkit-center;"><span style="text-align: -webkit-center;">(Tickets will then be opened to non-Meadows families)</span><br style="text-align: -webkit-center;"><span style="text-align: -webkit-center;">$15 adult / $5 child / Family of 2 adults &amp; 2 children&nbsp; - $30</span><br style="text-align: -webkit-center;"><br style="text-align: -webkit-center;"><span style="text-align: -webkit-center;">Purchasing information will be posted soon</span>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+
+            <div id="sidebar" class="span4">
+
+    <div id="top-staff" class="sidebar-section">
+        <h2 class="chrome">Executive Board 2012 - 2013</h2>
+        <ul class="list">
+
+                <li class="list-item last">
+                    <strong>President:</strong> <span>Woody Baltgalvis</span>
+                </li>
+
+                <li class="list-item last">
+                    <strong>Vice President:</strong> <span>Fadia Alaraj</span>
+                </li>
+
+                <li class="list-item last">
+                    <strong>Secretary:</strong> <span>Louanne Athanasiou</span>
+                </li>
+
+                <li class="list-item last">
+                    <strong>Treasurer:</strong> <span>Carlo Marcelo</span>
+                </li>
+
+                <li class="list-item last">
+                    <strong>Financial Secretary:</strong> <span>Pam Suboc</span>
+                </li>
+
+                <li class="list-item last">
+                    <strong>Historian:</strong> <span>Julian &amp; Annabelle Ponce</span>
+                </li>
+
+                <li class="list-item last">
+                    <strong>Parliamentarian:</strong> <span>Chris Mapa</span>
+                </li>
+
+                <li class="list-item last">
+                    <strong>Auditor:</strong> <span>Fiona Tam</span>
+                </li>
+
+        </ul>
+
+        <hr />
+                <h3>We Want You Involved!</h3>
+                Why join the PTA?
+
+                Research from the National PTA shows that when parents are involved, students have:
+                <ul>
+                    <li>Higher grades, test scores and graduation rates</li>
+                    <li>Better school attendance</li>
+                    <li>Increased motivation and self-esteem</li>
+                    <li>Lower rates of suspension</li>
+                    <li>Decreased use of alcohol and illegal drugs</li>
+                    <li>Fewer instances of violent behavior</li>
+                    <li>Greater enrollment rates in post-secondary education</li>
+                </ul>
+    </div>
+
+            </div>
+        </div>
+    </div>
+
+    </body>
+</html>
