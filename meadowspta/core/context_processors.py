ed1627d56870aedacbb9db5e72d198a6c5be0608
khalib <caleb.strumms@gmail.com>
2014-09-24 00:53:53 -0700

    Modified GA tag to use dynamic ID

--- meadowspta/core/context_processors.py
+++ meadowspta/core/context_processors.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 
+
 def view_variables(request):
     return {
+        'google_analytics_id': settings.GOOGLE_ANALYTICS_ID,
         'uri': 'http://%s%s' % (request.META['HTTP_HOST'], request.path),
         'meadows_map_url': 'https://maps.google.com/maps?q=1101+Helen+Drive+Millbrae,+CA+94030&hl=en&ll=37.602094,-122.418487&spn=0.004573,0.016158&sll=37.269174,-119.306607&sspn=10.362441,16.54541&hnear=1101+Helen+Dr,+Millbrae,+San+Mateo,+California+94030&t=m&z=16&layer=c&cbll=37.602093,-122.418482&panoid=eemM59uql4Hu_seTg4cBUg&cbp=11,217.17,,0,-4.18',
     }
--- meadowspta/middleware/view.py
+++ meadowspta/middleware/view.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponseRedirect
 
+
 class ViewInitMiddleware(object):
     def process_request(self, request):
         request.asdf = 'qwer'
         print "asdf"
         exit
-    
+
     def process_template_response(self, request, response):
         request.context['medows_root_url'] = 'qwer'
         print request
