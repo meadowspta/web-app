af5ba03689627ce5abafd987aa0eb4b413b560d1
khalib <caleb.strumms@gmail.com>
2014-11-13 01:15:55 -0800

    Added static and media directories

--- meadowspta/meadowspta/settings-prod.py
+++ meadowspta/meadowspta/settings-prod.py
@@ -59,7 +59,6 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'meadowspta.urls'
-
 WSGI_APPLICATION = 'meadowspta.wsgi.application'
 
 TEMPLATE_DIRS = (
@@ -94,8 +93,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Additional locations of static files
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Site settings.
 GOOGLE_ANALYTICS_ID = 'UA-36598051-3'
