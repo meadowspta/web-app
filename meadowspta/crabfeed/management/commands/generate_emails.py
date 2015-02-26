214a42f366e7764a3fd2f024c13b684dfce98d01
khalib <caleb.strumms@gmail.com>
2015-02-26 07:08:22 -0800

    Modified emails to be generated only for those who havent received it

--- meadowspta/crabfeed/management/commands/generate_emails.py
+++ meadowspta/crabfeed/management/commands/generate_emails.py
@@ -19,27 +19,29 @@ class Command(BaseCommand):
 
         reservations = Reservation.objects.all()
         for reservation in reservations:
-            names = []
-            for transaction in reservation.reservationtransaction_set.all():
-                names.append(transaction.name)
+            # Only generate emails for those who have not received it.
+            if reservation.email_sent is False:
+                names = []
+                for transaction in reservation.reservationtransaction_set.all():
+                    names.append(transaction.name)
 
-            token_map = {
-                'name': self.consolidate_names(names),
-                'email': reservation.email,
-                'reservation_number': reservation.reservation_number,
-                'party_count': reservation.party_count,
-                'qr_code': reservation.get_qr_code_url(),
-            }
+                token_map = {
+                    'name': self.consolidate_names(names),
+                    'email': reservation.email,
+                    'reservation_number': reservation.reservation_number,
+                    'party_count': reservation.party_count,
+                    'qr_code': reservation.get_qr_code_url(),
+                }
 
-            subject = EMAIL_SUBJECT % (reservation.reservation_number)
-            body = self.replace_email_body_tokens(token_map, EMAIL_BODY)
+                subject = EMAIL_SUBJECT % (reservation.reservation_number)
+                body = self.replace_email_body_tokens(token_map, EMAIL_BODY)
 
-            file_path = '%s/images/crabfeed/emails/%s.html' % (settings.MEDIA_ROOT, str(reservation.email))
+                file_path = '%s/images/crabfeed/emails/%s.html' % (settings.MEDIA_ROOT, str(reservation.email))
 
-            with open(file_path, 'w+') as file:
-                print 'Creating email file: %s' % file_path
-                file.write('%s\n\n%s' % (str(subject), str(body)))
-                file.close()
+                with open(file_path, 'w+') as file:
+                    print 'Creating email file: %s' % file_path
+                    file.write('%s\n\n%s' % (str(subject), str(body)))
+                    file.close()
 
 
     def replace_email_body_tokens(self, token_map, body):
