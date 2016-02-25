f5bae1c5a7327b861c3c160ba5541eddd13f2eb9
khalib <caleb.strumms@gmail.com>
2016-02-24 18:30:48 -0800

    Modified admin list and form output

--- meadowspta/auction/admin.py
+++ meadowspta/auction/admin.py
@@ -2,9 +2,34 @@ from django.contrib import admin
 from auction.models import Item
 
 class ItemAdmin(admin.ModelAdmin):
-    pass
-    # fields = ('title', 'description', 'is_published')
-    # list_display = ('title', 'description')
+    exclude = ('slug', 'item_number')
+    list_display = (
+        'title',
+        'get_image',
+        'get_description',
+        'item_value',
+        'contact',
+        'item_number',
+        'categories',
+        'get_starting_bid',
+        'get_buy_now_bid',
+        'bidding_end',
+    )
+
+    def get_description(self, item):
+        return item.description
+
+    def get_image(self, item):
+        return '<img src="%s">' % item.image_thumbnail.url
+
+    def get_starting_bid(self, item):
+        return '$%s' % item.starting_bid
+
+    def get_buy_now_bid(self, item):
+        return '$%s' % item.buy_now_bid
+
+    get_image.allow_tags = True
+    get_description.allow_tags = True
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'user':
