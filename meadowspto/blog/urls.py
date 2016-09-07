3f07aa1e0f57f40067a1a3079b4876690bcd2d7a
Caleb Whang <caleb.strumms@gmail.com>
2016-09-07 13:59:08 -0700

    Added blog feature route.

--- meadowspto/blog/urls.py
+++ meadowspto/blog/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.list),
     url(r'^(?P<slug>[^\.^/]+)/$', views.view),
-    # url(r'^feature/(?P<id>[^\.^/]+)/$', views.update_featured_post),
+    url(r'^feature/(?P<id>[^\.^/]+)/$', views.update_featured_post),
     # url(r'^get/(?P<id>[^\.^/]+)/$', views.get),
 ]
